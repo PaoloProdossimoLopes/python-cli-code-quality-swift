ab5196aaf469a19730bd9f76d95bd010e5e0295e
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-11-20 07:30:31 -0300

    feat: create a directory stats

---
+++ source/FileAnaliser.py
@@ -0,0 +1,112 @@
+from FileStats import FileStats
+
+class FileAnaliser:
+    __k_empty_text = ''
+    __k_spacing_text = ' '
+    __k_line_breaker = '\n'
+    
+    def __init__(self, page, page_path, page_name) -> None:
+        self.page = page
+        self.page_path = page_path
+        self.page_name = page_name
+
+    def count_number_of_imports(self) -> int:
+        import_identifier = 'import '
+        page = self.page
+        return page.count(import_identifier)
+
+    def count_number_of_class_declaration(self) -> int:
+        class_identifier = 'class '
+        page = self.page
+        return page.count(class_identifier)
+
+    def count_number_of_extensions_declared(self) -> int:
+        extension_identifier = 'extension '
+        page = self.page
+        return page.count(extension_identifier)
+
+    def count_number_of_lines(self) -> int:
+        page = self.page
+        lines = page.split(self.__k_line_breaker)
+        return len(lines)
+
+    def count_effective_number_of_code_lines(self) -> int:
+        page = self.page
+        page = page.replace(self.__k_spacing_text, self.__k_empty_text)
+        lines = page.split(self.__k_line_breaker)
+        while(self.__k_empty_text in lines):
+            lines.remove(self.__k_empty_text)
+        return len(lines)
+
+    def count_number_of_methods(self) -> int:
+        function_identifier = 'func '
+        page = self.page
+        return page.count(function_identifier)
+
+    def count_number_of_forceUnwrap(self) -> int:
+        force_unwrap_identifier = '! '
+        page = self.page
+        return page.count(force_unwrap_identifier)
+
+    def count_number_of_comments(self) -> int:
+        comment_identifier = '//'
+        page = self.page
+        return page.count(comment_identifier)
+
+    def count_number_of_todo_comments(self) -> int:
+        todo_comment_identifier = '//todo'
+        dash_identifier = '-'
+        page = self.page
+        page = page.replace(self.__k_spacing_text, self.__k_empty_text)
+        page = page.replace(dash_identifier, self.__k_empty_text)
+        page = page.lower()
+        return page.count(todo_comment_identifier)
+
+    def count_number_of_fix_comments(self) -> int:
+        fix_comment_identifier = '//fix'
+        page = self.page
+        page = page.replace(self.__k_spacing_text, self.__k_empty_text)
+        page = page.lower()
+        return page.count(fix_comment_identifier)
+
+    def count_number_of_unowned_reference(self) -> int:
+        unowned_identifier = '[unowned'
+        page = self.page
+        page = page.replace(self.__k_spacing_text, self.__k_empty_text)
+        page = page.lower()
+        return page.count(unowned_identifier)
+
+    def count_number_of_variable_assinging(self) -> int:
+        variable_identifier = 'var '
+        page = self.page
+        return page.count(variable_identifier)
+
+    def count_number_of_constant_assinging(self) -> int:
+        constant_identifier = 'let '
+        page = self.page
+        return page.count(constant_identifier)
+
+    def count_number_of_prints(self) -> int:
+        print_identifier = 'print('
+        page = self.page
+        return page.count(print_identifier)
+
+    def statistic(self) -> FileStats:
+        return FileStats(
+            path = self.page_path, 
+            name = self.page_name, 
+            number_of_imports = self.count_number_of_imports(),
+            number_of_class_namespaces = self.count_number_of_class_declaration(),
+            number_of_extensions = self.count_number_of_extensions_declared(),
+            number_of_lines = self.count_number_of_lines(),
+            number_of_effective_line_of_code = self.count_effective_number_of_code_lines(),
+            number_of_methods = self.count_number_of_methods(),
+            number_of_force_unwrap = self.count_number_of_forceUnwrap(),
+            number_of_comments = self.count_number_of_comments(),
+            number_of_todo_commnets = self.count_number_of_todo_comments(),
+            number_of_fix_commnets = self.count_number_of_fix_comments(),
+            number_of_unowned = self.count_number_of_unowned_reference(),
+            number_of_variable_declaration = self.count_number_of_variable_assinging(),
+            number_of_constant_declaration = self.count_number_of_constant_assinging(),
+            number_of_prints = self.count_number_of_prints()
+        )
--- source/main.py
+++ source/main.py
@@ -1,135 +1,97 @@
 import os
+from FileAnaliser import FileAnaliser
 from FileStats import FileStats
 
-class Analiser:
-    __k_empty_text = ''
-    __k_spacing_text = ' '
-    __k_line_breaker = '\n'
-    
-    def __init__(self, page, page_path, page_name) -> None:
-        self.page = page
-        self.page_path = page_path
-        self.page_name = page_name
+class DerectoryStats:
+    number_of_imports = 0
+    number_of_classes = 0
+    number_of_extension = 0
+    number_of_lines = 0
+    number_of_effective_line = 0
+    number_of_methods = 0
+    number_of_force_unwrap = 0
+    number_of_comments = 0
+    number_of_todo_commnets = 0
+    number_of_fix_commnets = 0
+    number_of_unowned = 0
+    number_of_variables = 0
+    number_of_constants = 0
+    number_of_prints = 0
 
-    def __count_number_of_imports(self) -> int:
-        import_identifier = 'import '
-        page = self.page
-        return page.count(import_identifier)
+    def __init__(self, route, list_of_file_stats) -> None:
+        self.route = route
+        self.list_of_file_stats = list_of_file_stats
+        self.calculate_all_stats_for_field()
 
-    def __count_number_of_class_declaration(self) -> int:
-        class_identifier = 'class '
-        page = self.page
-        return page.count(class_identifier)
+    def calculate_all_stats_for_field(self):
+        for file_stats in self.list_of_file_stats:
+            self.number_of_imports += file_stats.number_of_imports
+            self.number_of_classes += file_stats.number_of_class_namespaces
+            self.number_of_extension += file_stats.number_of_extensions
+            self.number_of_lines += file_stats.number_of_lines
+            self.number_of_effective_line += file_stats.number_of_effective_line_of_code
+            self.number_of_methods += file_stats.number_of_methods
+            self.number_of_force_unwrap += file_stats.number_of_force_unwrap
+            self.number_of_comments += file_stats.number_of_comments
+            self.number_of_todo_commnets += file_stats.number_of_todo_commnets
+            self.number_of_fix_commnets += file_stats.number_of_fix_commnets
+            self.number_of_unowned += file_stats.number_of_unowned
+            self.number_of_variables += file_stats.number_of_variable_declaration
+            self.number_of_constants += file_stats.number_of_constant_declaration
+            self.number_of_prints += file_stats.number_of_prints
 
-    def __count_number_of_extensions_declared(self) -> int:
-        extension_identifier = 'extension '
-        page = self.page
-        return page.count(extension_identifier)
-
-    def __count_number_of_lines(self) -> int:
-        page = self.page
-        lines = page.split(self.__k_line_breaker)
-        return len(lines)
-
-    def __count_effective_number_of_code_lines(self) -> int:
-        page = self.page
-        page = page.replace(self.__k_spacing_text, self.__k_empty_text)
-        lines = page.split(self.__k_line_breaker)
-        while(self.__k_empty_text in lines):
-            lines.remove(self.__k_empty_text)
-        return len(lines)
-
-    def __count_number_of_methods(self) -> int:
-        function_identifier = 'func '
-        page = self.page
-        return page.count(function_identifier)
-
-    def __count_number_of_forceUnwrap(self) -> int:
-        force_unwrap_identifier = '! '
-        page = self.page
-        return page.count(force_unwrap_identifier)
-
-    def __count_number_of_comments(self) -> int:
-        comment_identifier = '//'
-        page = self.page
-        return page.count(comment_identifier)
-
-    def __count_number_of_todo_comments(self) -> int:
-        todo_comment_identifier = '//todo'
-        dash_identifier = '-'
-        page = self.page
-        page = page.replace(self.__k_spacing_text, self.__k_empty_text)
-        page = page.replace(dash_identifier, self.__k_empty_text)
-        page = page.lower()
-        return page.count(todo_comment_identifier)
-
-    def __count_number_of_fix_comments(self) -> int:
-        fix_comment_identifier = '//fix'
-        page = self.page
-        page = page.replace(self.__k_spacing_text, self.__k_empty_text)
-        page = page.lower()
-        return page.count(fix_comment_identifier)
-
-    def __count_number_of_unowned_reference(self) -> int:
-        unowned_identifier = '[unowned'
-        page = self.page
-        page = page.replace(self.__k_spacing_text, self.__k_empty_text)
-        page = page.lower()
-        return page.count(unowned_identifier)
-
-    def __count_number_of_variable_assinging(self) -> int:
-        variable_identifier = 'var '
-        page = self.page
-        return page.count(variable_identifier)
-
-    def __count_number_of_constant_assinging(self) -> int:
-        constant_identifier = 'let '
-        page = self.page
-        return page.count(constant_identifier)
-
-    def __count_number_of_prints(self) -> int:
-        print_identifier = 'print('
-        page = self.page
-        return page.count(print_identifier)
-
-    def statistic(self) -> FileStats:
-        return FileStats(
-            path = self.page_path, 
-            name = self.page_name, 
-            number_of_imports = self.__count_number_of_imports(),
-            number_of_class_namespaces = self.__count_number_of_class_declaration(),
-            number_of_extensions = self.__count_number_of_extensions_declared(),
-            number_of_lines = self.__count_number_of_lines(),
-            number_of_effective_line_of_code = self.__count_effective_number_of_code_lines(),
-            number_of_methods = self.__count_number_of_methods(),
-            number_of_force_unwrap = self.__count_number_of_forceUnwrap(),
-            number_of_comments = self.__count_number_of_comments(),
-            number_of_todo_commnets = self.__count_number_of_todo_comments(),
-            number_of_fix_commnets = self.__count_number_of_fix_comments(),
-            number_of_unowned = self.__count_number_of_unowned_reference(),
-            number_of_variable_declaration = self.__count_number_of_variable_assinging(),
-            number_of_constant_declaration = self.__count_number_of_constant_assinging(),
-            number_of_prints = self.__count_number_of_prints()
-        )
+    def __str__(self) -> str:
+        route = self.route
+        list_of_path = route.split('/')
+        last_index = len(list_of_path) - 1
+        director_name = list_of_path[last_index]
+        header = f'* DIRECTOR: {director_name} *'
+        separator = len(header) * '*'
+        return f'''
+        {header}
+        {separator}
+        - PATH: {route}
+        - NUMBER OF IMPORTS: {self.number_of_imports}
+        - NUMBER OF CLASSES: {self.number_of_classes}
+        - NUMBER OF EXTENSIONS: {self.number_of_extension}
+        - NUMBER OF LINES (LOC): {self.number_of_lines}
+        - NUMBER OF EFFECTIVE LINES: {self.number_of_effective_line}
+        - NUMBER OF METHODS: {self.number_of_methods}
+        - NUMBER OF FORCE UNWRAP (!): {self.number_of_force_unwrap}
+        - NUMBER OF COMMENTS (ALL TIPES): {self.number_of_comments}
+        - NUMBER OF TO-DO COMMENTS: {self.number_of_todo_commnets}
+        - NUMBER OF FIX COMMENTS: {self.number_of_fix_commnets}
+        - NUMBER OF UNOWNED REFERENCES: {self.number_of_unowned}
+        - NUMBER OF VARIABLES DECALRED (var): {self.number_of_variables}
+        - NUMBER OF CONSTANTS DECALRED (let): {self.number_of_constants}
+        - NUMBER OF PRINTS: {self.number_of_prints}
+        '''
 
 # route example => /Users/prols/Arquivos/dev/github-repos/swift/health-tracker-app/health-tracker/App/FitFica/Source/Features/Scenes/Login/Model/
-K_SWIFT_EXTENSION = '.swift'
-READ_FILE_MODE = 'r'
-route = input('Path: ')
-initial_directory = "."
-file_with_properties = []
-for root, dirs, files in os.walk(route + initial_directory):
-    for name in files:
-        if name.endswith(K_SWIFT_EXTENSION):
-            path = f'{root}/{name}'
-            with open(path, READ_FILE_MODE) as file:
-                page = file.read()
-                analiser = Analiser(page, path, name)
-                object = analiser.statistic()
-                file_with_properties.append(object)
+def analise_all_font(route):
+    K_SWIFT_EXTENSION = '.swift'
+    READ_FILE_MODE = 'r'
+    initial_directory = ""
+    file_with_properties = []
+    for root, dirs, files in os.walk(route + initial_directory):
+        for name in files:
+            if name.endswith(K_SWIFT_EXTENSION):
+                path = f'{root}/{name}'
+                with open(path, READ_FILE_MODE) as file:
+                    page = file.read()
+                    analiser = FileAnaliser(page, path, name)
+                    object = analiser.statistic()
+                    file_with_properties.append(object)
+    return file_with_properties
+
+def print_stats(file_stats: FileStats):
+    for stats in file_stats:
+        print(stats)
 
 
-# IMPROMINDO
-for file_stats in file_with_properties:
-    print(file_stats)
-            
+if __name__ == '__main__':
+    # route = input('Path: ')
+    route = '/Users/prols/Arquivos/dev/github-repos/essential-feed-case-study/EssentialFeed/EssentialFeed'
+    file_stats = analise_all_font(route)
+    print(DerectoryStats(route, file_stats))
+    # print_stats(file_stats)
