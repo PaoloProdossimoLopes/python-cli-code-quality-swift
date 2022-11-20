803e56b7e5c8cdea558a6c3a3c2cd1065ef81aa2
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-11-20 01:30:59 -0300

    refactor: improve redability appling clean code and constant single source of truthy

--- source/main.py
+++ source/main.py
@@ -43,7 +43,7 @@ class FileStatisticDTO:
         - NUMBER OF LINES (LOC): {self.number_of_lines}
         - NUMBER OF EFFECTIVE LINES: {self.number_of_effective_line_of_code}
         - NUMBER OF METHODS: {self.number_of_methods}
-        - NUMBER OF FORCE UNWRAP (1): {self.number_of_force_unwrap}
+        - NUMBER OF FORCE UNWRAP (!): {self.number_of_force_unwrap}
         - NUMBER OF COMMENTS (ALL TIPES): {self.number_of_comments}
         - NUMBER OF TO-DO COMMENTS: {self.number_of_todo_commnets}
         - NUMBER OF FIX COMMENTS: {self.number_of_fix_commnets}
@@ -54,76 +54,92 @@ class FileStatisticDTO:
         '''
 
 class Analiser:
+    __k_empty_text = ''
+    __k_spacing_text = ' '
+    __k_line_breaker = '\n'
     
     def __init__(self, page, page_path, page_name) -> None:
         self.page = page
         self.page_path = page_path
         self.page_name = page_name
 
-    def count_number_of_imports(self) -> int:
-        return self.page.count('import ')
+    def __count_number_of_imports(self) -> int:
+        import_identifier = 'import '
+        return self.page.count(import_identifier)
 
-    def count_number_of_class_declaration(self) -> int:
-        return self.page.count('class ')
+    def __count_number_of_class_declaration(self) -> int:
+        class_identifier = 'class '
+        return self.page.count(class_identifier)
 
-    def count_number_of_extensions_declared(self) -> int:
-        return self.page.count('extension ')
+    def __count_number_of_extensions_declared(self) -> int:
+        extension_identifier = 'extension '
+        return self.page.count(extension_identifier)
 
-    def count_number_of_lines(self) -> int:
-        lines = self.page.split('\n')
+    def __count_number_of_lines(self) -> int:
+        lines = self.page.split(self.__k_line_breaker)
         return len(lines)
 
-    def count_effective_number_of_code_lines(self) -> int:
-        lines = self.page.replace(' ', '').split('\n')
-        while('' in lines):
-            lines.remove('')
+    def __count_effective_number_of_code_lines(self) -> int:
+        lines = self.page.replace(self.__k_spacing_text, self.__k_empty_text).split(self.__k_line_breaker)
+        while(self.__k_empty_text in lines):
+            lines.remove(self.__k_empty_text)
         return len(lines)
 
-    def count_number_of_methods(self) -> int:
-        return self.page.count('func ')
+    def __count_number_of_methods(self) -> int:
+        function_identifier = 'func '
+        return self.page.count(function_identifier)
 
-    def count_number_of_forceUnwrap(self) -> int:
-        return self.page.count('! ')
+    def __count_number_of_forceUnwrap(self) -> int:
+        force_unwrap_identifier = '! '
+        return self.page.count(force_unwrap_identifier)
 
-    def count_number_of_comments(self) -> int:
-        return self.page.count('//')
+    def __count_number_of_comments(self) -> int:
+        comment_identifier = '//'
+        return self.page.count(comment_identifier)
 
-    def count_number_of_todo_comments(self) -> int:
-        return self.page.replace(' ', '').replace('-', '').lower().count('//todo')
+    def __count_number_of_todo_comments(self) -> int:
+        todo_comment_identifier = '//todo'
+        dash_identifier = '-'
+        return self.page.replace(self.__k_spacing_text, self.__k_empty_text).replace(dash_identifier, self.__k_empty_text).lower().count(todo_comment_identifier)
 
-    def count_number_of_fix_comments(self) -> int:
-        return self.page.replace(' ', '').lower().count('//fix')
+    def __count_number_of_fix_comments(self) -> int:
+        fix_comment_identifier = '//fix'
+        return self.page.replace(self.__k_spacing_text, self.__k_empty_text).lower().count(fix_comment_identifier)
 
-    def count_number_of_unowned_reference(self) -> int:
-        return self.page.replace(' ', '').lower().count('[unowned')
+    def __count_number_of_unowned_reference(self) -> int:
+        unowned_identifier = '[unowned'
+        return self.page.replace(self.__k_spacing_text, self.__k_empty_text).lower().count(unowned_identifier)
 
-    def count_number_of_variable_assinging(self) -> int:
-        return self.page.count('var ')
+    def __count_number_of_variable_assinging(self) -> int:
+        variable_identifier = 'var '
+        return self.page.count(variable_identifier)
 
-    def count_number_of_constant_assinging(self) -> int:
-        return self.page.count('let ')
+    def __count_number_of_constant_assinging(self) -> int:
+        constant_identifier = 'let '
+        return self.page.count(constant_identifier)
 
-    def count_number_of_prints(self) -> int:
-        return self.page.count('print(')
+    def __count_number_of_prints(self) -> int:
+        print_identifier = 'print('
+        return self.page.count(print_identifier)
 
     def statistic(self) -> FileStatisticDTO:
         return FileStatisticDTO(
-            path=self.page_path, 
-            name=self.page_name, 
-            number_of_imports=self.count_number_of_imports(),
-            number_of_class_namespaces=self.count_number_of_class_declaration(),
-            number_of_extensions=self.count_number_of_extensions_declared(),
-            number_of_lines=self.count_number_of_lines(),
-            number_of_effective_line_of_code=self.count_effective_number_of_code_lines(),
-            number_of_methods=self.count_number_of_methods(),
-            number_of_force_unwrap=self.count_number_of_forceUnwrap(),
-            number_of_comments=self.count_number_of_comments(),
-            number_of_todo_commnets=self.count_number_of_todo_comments(),
-            number_of_fix_commnets=self.count_number_of_fix_comments(),
-            number_of_unowned=self.count_number_of_unowned_reference(),
-            number_of_variable_declaration=self.count_number_of_variable_assinging(),
-            number_of_constant_declaration=self.count_number_of_constant_assinging(),
-            number_of_prints=self.count_number_of_prints()
+            path = self.page_path, 
+            name = self.page_name, 
+            number_of_imports = self.__count_number_of_imports(),
+            number_of_class_namespaces = self.__count_number_of_class_declaration(),
+            number_of_extensions = self.__count_number_of_extensions_declared(),
+            number_of_lines = self.__count_number_of_lines(),
+            number_of_effective_line_of_code = self.__count_effective_number_of_code_lines(),
+            number_of_methods = self.__count_number_of_methods(),
+            number_of_force_unwrap = self.__count_number_of_forceUnwrap(),
+            number_of_comments = self.__count_number_of_comments(),
+            number_of_todo_commnets = self.__count_number_of_todo_comments(),
+            number_of_fix_commnets = self.__count_number_of_fix_comments(),
+            number_of_unowned = self.__count_number_of_unowned_reference(),
+            number_of_variable_declaration = self.__count_number_of_variable_assinging(),
+            number_of_constant_declaration = self.__count_number_of_constant_assinging(),
+            number_of_prints = self.__count_number_of_prints()
         )
 
 # route example => /Users/prols/Arquivos/dev/github-repos/swift/health-tracker-app/health-tracker/App/FitFica/Source/Features/Scenes/Login/Model/
